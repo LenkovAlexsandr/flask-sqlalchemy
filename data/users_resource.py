de7186d01f3a5ac66ad7c405789c768b2463875a
mapProject <lenkovsaschka@gmail.com>
2024-03-04 16:52:49 +0300

    5

---
+++ data/users_resource.py
@@ -0,0 +1,58 @@
+from flask_restful import abort, Resource
+from . import db_session
+from .users import User
+from flask import jsonify
+from .reqparse_user import parser
+
+
+def abort_if_users_not_found(users_id):
+    session = db_session.create_session()
+    users = session.query(User).get(users_id)
+    if not users:
+        abort(404, message=f"Users {users_id} not found")
+
+
+class UsersResource(Resource):
+    def get(self, users_id):
+        abort_if_users_not_found(users_id)
+        session = db_session.create_session()
+        users = session.query(User).get(users_id)
+        return jsonify({'users': users.to_dict(
+            only=('id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email',
+                  'hashed_password', 'modified_date'))})
+
+    def delete(self, users_id):
+        abort_if_users_not_found(users_id)
+        session = db_session.create_session()
+        users = session.query(User).get(users_id)
+        session.delete(users)
+        session.commit()
+        return jsonify({'success': 'OK'})
+
+
+class UsersListResource(Resource):
+    def get(self):
+        session = db_session.create_session()
+        users = session.query(User).all()
+        return jsonify({'users': [item.to_dict(
+            only=('id', 'surname', 'name', 'age', 'position', 'speciality', 'address', 'email',
+                  'hashed_password', 'modified_date')) for item in users]})
+
+    def post(self):
+        args = parser.parse_args()
+        session = db_session.create_session()
+        users = User(
+            id=args['id'],
+            surname=args['surname'],
+            name=args['name'],
+            age=args['age'],
+            position=args['position'],
+            speciality=args['speciality'],
+            address=args['address'],
+            email=args['email'],
+            hashed_password=args['hashed_password'],
+            modified_date=args['modified_date']
+        )
+        session.add(users)
+        session.commit()
+        return jsonify({'id': users.id})
--- main.py
+++ main.py
@@ -4,44 +4,22 @@ from forms.login import LoginForm
 from data.users import User
 from data.news import News
 from data import db_session, news_api
+from flask_restful import Api
+from data import news_resources
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-login_manager = LoginManager()
-login_manager.init_app(app)
+api = Api(app)
+# для списка объектов
+api.add_resource(news_resources.NewsListResource, '/api/v2/news')
+# для одного объекта
+api.add_resource(news_resources.NewsResource, '/api/v2/news/<int:news_id>')
 
 
 def main():
     db_session.global_init("db/blogs.db")
     app.register_blueprint(news_api.blueprint)
     app.run()
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    db_sess = db_session.create_session()
-    return db_sess.query(User).get(user_id)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
-
-@app.route('/')
-def base():
-    return render_template('base.html')
 
 
 if __name__ == '__main__':
